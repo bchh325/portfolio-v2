760bbd6014d2a91af801a6d082930ea7a7c0becb
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-01-18 00:45:23 -0800

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './css_modules/Nav.module.css'
+
+export default function Nav() {
+  return (
+    <div className={styles["nav-container"]}>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+    </div>
+  )
+}
