73ce6c884dc267596bd7c477bb34e099bef05588
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-01-18 23:06:27 -0800

    css animations, changed font family

--- src/components/IconLinks.jsx
+++ src/components/IconLinks.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './css_modules/IconLinks.module.css'
+import { AiOutlineLinkedin, AiFillGithub } from "react-icons/ai"
 
 export default function IconLinks() {
+
+  const clickHandler = () => {
+    console.log("click")
+  }
+
   return (
     <div className={styles["icons-container"]}>
-        <span>LinkedIn</span>
-        <span>GitHub</span>
+      <AiOutlineLinkedin onClick={clickHandler} className={styles.icon} size={50}/>
+      <AiFillGithub onClick={clickHandler} className={styles.icon} size={50}/>
     </div>
   )
 }
