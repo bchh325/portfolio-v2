594dda3ca8f0955b0a8c9a3dbd48fce5d4e74d79
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-01-19 03:51:59 -0800

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './css_modules/Footer.module.css'
 export default function Footer() {
   return (
     <div className={styles["footer-container"]}>
-        <span className={styles.info}>Created using React by Bryan Chhorb</span>
+        <span className={styles.info}>Created from scratch using React by Bryan Chhorb</span>
     </div>
   )
 }
