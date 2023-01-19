412c62ae78f5083d5ab1b130db776dab4a6c3a88
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-01-19 02:44:02 -0800

    personal information styling

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -4,9 +4,39 @@ import styles from './css_modules/PersonalInfo.module.css'
 export default function PersonalInfo() {
   return (
     <div className={styles["personalinfo-container"]}>
-      <span>Profile Picture Placeholder</span>
-      <span>About Me Placeholder</span>
-      <span>Languages Placeholder</span>
+      <span className={styles.name}>Bryan Chhorb</span>
+      <div className={styles["about-me"]}>
+        <p>
+          Aspiring developer that is motivated by the desire to learn the latest technologies and by having fun thinking through programmatic problems as if it were a jigsaw puzzle.
+        </p>
+        <p>
+          I have always loved the thought process that goes behind solving mathematical problems and I was delighted to find that developing applications requires the same line of reasoning to achieve a desired result.
+        </p>
+      </div>
+      <div className={styles["tech-container"]}>
+        <div>
+          <span>Languages<br /></span>
+          <span className={styles.languages}>
+            React JSX -
+            Node.js -
+            HTML -
+            CSS -
+            Python -
+            SQL
+          </span>
+        </div>
+        <div>
+          <span>Technologies Used<br /></span>
+          <span className={styles.languages}>
+            AWS: Lambda -
+            API Gateway -
+            DynamoDB -
+            Cognito -
+            Secrets Manager -
+            IAM
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
