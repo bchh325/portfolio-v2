325afccb1a50da80e4c7fe39dd99dc99ab2aff75
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-01-19 03:16:51 -0800

    added links to linkedin and github

--- src/components/IconLinks.jsx
+++ src/components/IconLinks.jsx
@@ -4,16 +4,15 @@ import { AiOutlineLinkedin, AiFillGithub } from "react-icons/ai"
 import FadeIn from './FadeIn'
 
 export default function IconLinks() {
-
-  const clickHandler = () => {
-    console.log("click")
-  }
-
   return (
     <FadeIn>
       <div className={styles["icons-container"]}>
-        <AiOutlineLinkedin onClick={clickHandler("linkedin")} className={styles.icon} size={50} />
-        <AiFillGithub onClick={clickHandler("github")} className={styles.icon} size={50} />
+        <a href="https://www.linkedin.com/in/bryanmtxd/" target="_blank">
+          <AiOutlineLinkedin className={styles.icon} size={50} />
+        </a>
+        <a href="https://github.com/bchh325" target="_blank">
+          <AiFillGithub className={styles.icon} size={50} />
+        </a>
       </div>
     </FadeIn>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,22 +6,16 @@ export default function Projects() {
   return (
     <div className={styles["projects-flex-container"]}>
       <FadeIn>
-        <div className={`${styles.box} ${styles["first-project"]}`}>Project</div>
+        <div className={`${styles.box} ${styles["first-project"]}`}>Placeholder Text. Modal comes later</div>
       </FadeIn>
       <FadeIn>
-        <div className={`${styles.box} ${styles["second-project"]}`}>Project</div>
+        <div className={`${styles.box} ${styles["second-project"]}`}>Placeholder Text. Modal comes later</div>
       </FadeIn>
       <FadeIn>
-        <div className={styles.box}>Project</div>
+        <div className={styles.box}>Placeholder Text.</div>
       </FadeIn>
       <FadeIn>
-        <div className={styles.box}>Project</div>
-      </FadeIn>
-      <FadeIn>
-        <div className={styles.box}>Project</div>
-      </FadeIn>
-      <FadeIn>
-        <div className={styles.box}>Project</div>
+        <div className={styles.box}>Placeholder Text.</div>
       </FadeIn>
     </div>
   )
