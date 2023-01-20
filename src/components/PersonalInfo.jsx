4e1b2fd1fec263cff7c99eda5bda43074f189aa7
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-01-19 21:48:02 -0800

    changed wording

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -7,7 +7,7 @@ export default function PersonalInfo() {
       <span className={styles.name}>Bryan Chhorb</span>
       <div className={styles["about-me"]}>
         <p>
-          Aspiring developer that is motivated by the desire to learn the latest technologies and by having fun thinking through programmatic problems as if it were a jigsaw puzzle.
+          Aspiring developer that is motivated by the desire to learn the latest technologies and by having fun thinking through programming problems as if it were a jigsaw puzzle.
         </p>
         <p>
           I have always loved the thought process that goes behind solving mathematical problems and I was delighted to find that developing applications requires the same line of reasoning to achieve a desired result.
